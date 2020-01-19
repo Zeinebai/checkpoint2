582355b6801c7f4056d2b69e3d649764f55d905f
Zeinebai <56039447+Zeinebai@users.noreply.github.com>
2020-01-19 11:57:37 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -27,10 +27,86 @@ function changetounderlined() {
     }
 }
 var changeFont = function (font) {
-    console.log(font.value)
-    document.getElementById("output-text").style.fontFamily = font.value;
+
+    document.getElementById("Mytext").style.fontFamily = document.getElementById("input-font").value;
 }
 
 function changesize() {
-    document.getElementById("output-text").style.fontSize = document.getElementById("fontsize").value;
+    document.getElementById("Mytext").style.fontSize = document.getElementById("fontsize").value;
 }
+
+$(".container a").css("opacity", "0");
+
+$(".container").mouseenter(function () {
+    str = "#" + this.id + " a";
+    $(str).css("opacity", "1");
+});
+
+$(".container").mouseleave(function () {
+    str = "#" + this.id + " a";
+    $(str).css("opacity", "0");
+});
+$("#im1").css("opacity", "1");
+
+$("#im1").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im1").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im2").css("opacity", "1");
+
+$("#im2").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im2").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im3").css("opacity", "1");
+
+$("#im3").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im3").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im4").css("opacity", "1");
+
+$("#im4").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im4").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im5").css("opacity", "1");
+
+$("#im5").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im5").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im6").css("opacity", "1");
+
+$("#im6").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im6").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im7").css("opacity", "1");
+
+$("#im7").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im7").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
+$("#im8").css("opacity", "1");
+
+$("#im8").mouseenter(function () {
+    $(this).css("opacity", "0.5");
+});
+$("#im8").mouseleave(function () {
+    $(this).css("opacity", "1");
+});
